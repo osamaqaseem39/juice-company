77c242e59590ad89d729607895d2caff32bd43d3
osamaqaseem39 <osama.qa.39@gmail.com>
2025-08-08 17:31:52 +0500

    Fix nodemailer method name - createTransport instead of createTransporter

--- natureharvest-admin/client/src/pages/Categories/CategoryForm.tsx
+++ natureharvest-admin/client/src/pages/Categories/CategoryForm.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useCreateCategory, useUpdateCategory, useCategory, useCategories } from '../../services/apiService';
+import { useCreateCategory, useUpdateCategory, useCategory } from '../../services/apiService';
 import { 
   FormField, 
   ImageUpload, 
