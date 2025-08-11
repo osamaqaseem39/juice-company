5937743c3cb23787caf6ce4bb6f0e6653ae41339
osamaqaseem39 <osama.qa.39@gmail.com>
2025-08-11 13:40:54 +0500

    Fix ESLint errors: Remove unused imports from SubCategoryForm.tsx

--- natureharvest-admin/client/src/pages/Categories/SubCategoryForm.tsx
+++ natureharvest-admin/client/src/pages/Categories/SubCategoryForm.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { categoryApi, subcategoryApi, Category } from '../../services/api';
-import { useCreateSubcategory, useUpdateSubcategory, useSubcategory } from '../../services/apiService';
-import { useCategories } from '../../hooks/useCategories';
+
 import { UPLOAD_URL } from '../../config/env';
 
 const initialState = {
